5919fe29c78f138ee2c4b82e9715cfc59f8a9f15
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-07-28 11:38:55 +0200

    Commands and game over and constraints added

---
+++ app/src/main/java/vcmsa/projects/icetask1/Commands.kt
@@ -0,0 +1,8 @@
+package vcmsa.projects.icetask1
+
+data class Commands (
+    val RedCommand: String= "Click red",
+    val YellowCommand: String = "Click yellow",
+    val GreenCommand: String= "Click green",
+    val BlueCommand: String = "Click blue"
+)
