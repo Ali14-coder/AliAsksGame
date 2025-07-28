0d8a72936c2714bd401316f752aa8e80bf184913
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-07-28 12:25:53 +0200

    Button handler

--- app/src/main/java/vcmsa/projects/icetask1/Commands.kt
+++ app/src/main/java/vcmsa/projects/icetask1/Commands.kt
@@ -4,5 +4,20 @@ data class Commands (
     val RedCommand: String= "Click red",
     val YellowCommand: String = "Click yellow",
     val GreenCommand: String= "Click green",
-    val BlueCommand: String = "Click blue"
+    val BlueCommand: String = "Click blue",
+
+    val RedCommand2: String  ="Don't click Yellow, Green or Blue.",
+    val YellowCommand2: String  ="Don't click Red, Blue or Green.",
+    val GreenCommand2: String  ="Don't click Yellow, Red or Blue.",
+    val BlueCommand2: String  ="Don't click Green, Yellow or Blue.",
+
+    val RedCommand3: String  ="Click the first colour of the rainbow",
+    val YellowCommand3: String  ="Click the main colour of Spongebob.",
+    val GreenCommand3: String  ="Click the colour of grass.",
+    val BlueCommand3: String  ="Click the colour of the ocean.",
+
+    val RedCommand4: String  ="Click the colour that ryhmes with 'bed'.",
+    val YellowCommand4: String  ="Click the colour that rhymes with 'mellow'.",
+    val GreenCommand4: String  ="Click the colour that sounds like 'mean'.",
+    val BlueCommand4: String  ="Click the colour that rhymes with 'flu.' ",
 )
--- app/src/main/java/vcmsa/projects/icetask1/MainActivity.kt
+++ app/src/main/java/vcmsa/projects/icetask1/MainActivity.kt
@@ -1,6 +1,10 @@
 package vcmsa.projects.icetask1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +21,48 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    val btnRed = findViewById<Button>(R.id.btnRed)
+    val btnYellow = findViewById<Button>(R.id.btnYellow)
+    val btnBlue = findViewById<Button>(R.id.btnBlue)
+    val btnGreen = findViewById<Button>(R.id.btnGreen)
+
+    val txtCommand = findViewById<TextView>(R.id.txtCommand)
+
+//    val RedColour = "Red"
+//    val YellowColour = "Yellow"
+//    val BlueColour = "Blue"
+//    val GreenColour = "Green"
+
+    btnRed.setOnClickListener{
+        view -> handleButtonClick(view)
+        Toast.makeText(this, "Red button was clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun handleButtonClick(view: View){
+        when (view.id) {
+            R.id.btnRed -> {
+                if (txtCommand = RedCommand || txtCommand = RedCommand2 || txtCommand = RedCommand3 || txtCommand = RedCommand4){
+
+                }
+            }
+            R.id.btnYellow -> {
+                if (txtCommand = YellowCommand || txtCommand = YellowCommand2 || txtCommand = YellowCommand3 || txtCommand = YellowCommand4){
+
+                }
+            }
+            R.id.btnGreen -> {
+                if (txtCommand = GreenCommand || txtCommand = GreenCommand2 || txtCommand = GreenCommand3 || txtCommand = GreenCommand4){
+
+                }
+            }
+            R.id.btnBlue -> {
+                if (txtCommand = BlueCommand || txtCommand = BlueCommand2 || txtCommand = BlueCommand3 || txtCommand = BlueCommand4){
+
+                }
+            }
+
+        }
+    }
+
 
 }
